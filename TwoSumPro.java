84e9d85f3a1c4c4b6a2e3729027a5869df441fac
Ansarshirzad2r382 <106185914+Ansarshirzad2r382@users.noreply.github.com>
2024-12-04 00:14:10 +0100

    Update TwoSumPro.java

--- TwoSumPro.java
+++ TwoSumPro.java
@@ -1,7 +1,41 @@
 
+
+1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+approach way: there are 2 ways to solve this problem
+              1- use 2 loops, the first loop, from the 1st loop we take a digit 
+                 and from the 2nd loop we take each i+1 digit, if the sum of digit ith pos and jth position = target then we have our solution else we should take another number.  
+              2- we can also reach the solution with one loop O(n)
+*/
+     
+     
+
 import java.util.Arrays;
 class TwoSumPro{
 
+     
      public int[] twoSum(int[] nums, int target) {
         for(int i=0; i<= nums.length; ++i){
             for(int j= i +1 ; j < nums.length; j++){
@@ -12,6 +46,23 @@ class TwoSumPro{
         }
         return null; 
     }
+    // Optimierte Loesung 2: das eingegebene Array muss dafür sortiert sein.  
+     public int[] twoSumSorted(int[] nums, int target) {
+    int left = 0, right = nums.length - 1;
+    while (left < right) {
+        int sum = nums[left] + nums[right];
+        if (sum == target) {
+            return new int[] {left, right};
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return null;
+}
+
+     
     public static void main(String[] args ){
         TwoSumPro a = new TwoSumPro();
         int[] erg  = a.twoSum(new int[]{2,7,11,15}, 9);
